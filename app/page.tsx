d457f989960a4de5116c850b94bb7de52ad0cca0
Sangita Papola <sangitapapola@gmail.com>
2025-07-18 19:11:48 +0530

    working file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Page from "./yolov8n/object_detection.js"
+import Page from "./yolov8n/object.js"
 export default function Home() {
   return (
    <div><Page/></div>
